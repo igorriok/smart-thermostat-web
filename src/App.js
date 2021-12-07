8f5e4b924cafa133b34fa38022b8f11f500e3cee
Igor <i.solonari@gmail.com>
2021-12-07 16:08:08 +0000

    connect backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-const baseUrl = "http://raspberypi.local";
+const baseUrl = "http://raspberrypi.local:30578";
 
 function App() {
 
-  const houseTemp = 23;
-  const themperatureSet = 24;
-  const humidity = 0;
-  const pressure = 0;
+  const [ houseTemp, setHouseTemp ] = useState(0);
+  const [ themperatureSet, setThemperatureSet ] = useState(0);
+  const [ humidity, setHumidity ] = useState(0);
+  const [ pressure, setPressure ] = useState(0);
+
+  useEffect(() => {
+
+    fetch(baseUrl,
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Accept": "application/json",
+        },
+      })
+      .then((response) => {
+        // console.log(response);
+        return response.json();
+      },
+      )
+      .then((data) => {
+        // console.log(data);
+        
+        setThemperatureSet(data.themperature);
+      })
+      .catch((error) => {
+        console.error("Could not get data from server");
+        console.error(error);
+      });
+
+  },[]);
 
   return (
     <div className="App">
@@ -42,7 +72,7 @@ function App() {
       <div id="themperatureSetContainer">
         <h4>
           {
-            themperatureSet
+            "Wanted themperature: " + themperatureSet
           }
         </h4>
       </div>
