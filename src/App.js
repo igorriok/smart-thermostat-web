292e7c9f5e30136955879536cc3257469e293678
Igor <i.solonari@gmail.com>
2021-12-08 10:08:00 +0000

    get data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const baseUrl = "http://raspberrypi.local:30578";
 function App() {
 
   const [ houseTemp, setHouseTemp ] = useState(0);
-  const [ themperatureSet, setThemperatureSet ] = useState(0);
+  const [ temperatureSet, setTemperatureSet ] = useState(0);
   const [ humidity, setHumidity ] = useState(0);
   const [ pressure, setPressure ] = useState(0);
 
@@ -31,7 +31,38 @@ function App() {
       .then((data) => {
         // console.log(data);
         
-        setThemperatureSet(data.themperature);
+        setTemperatureSet(data.temperature);
+      })
+      .catch((error) => {
+        console.error("Could not get data from server");
+        console.error(error);
+      });
+
+  },[]);
+
+  useEffect(() => {
+
+    fetch(baseUrl + "/data",
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Accept": "application/json",
+        },
+      })
+      .then((response) => {
+        // console.log(response);
+        return response.json();
+      },
+      )
+      .then((data) => {
+        // console.log(data);
+        
+        setHouseTemp(data.temperature);
+        setPressure(data.pressure);
+        setHumidity(data.humidity);
       })
       .catch((error) => {
         console.error("Could not get data from server");
@@ -72,7 +103,7 @@ function App() {
       <div id="themperatureSetContainer">
         <h4>
           {
-            "Wanted themperature: " + themperatureSet
+            "Wanted themperature: " + temperatureSet
           }
         </h4>
       </div>
