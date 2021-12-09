6dab009371c8e17471a2759cad69ef1e15c2360d
Igor <i.solonari@gmail.com>
2021-12-09 12:00:44 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,113 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './pages/Home';
 
 const baseUrl = "http://raspberrypi.local:30578";
 
 function App() {
 
-  const [ houseTemp, setHouseTemp ] = useState(0);
-  const [ temperatureSet, setTemperatureSet ] = useState(0);
-  const [ humidity, setHumidity ] = useState(0);
-  const [ pressure, setPressure ] = useState(0);
-
-  useEffect(() => {
-
-    fetch(baseUrl,
-      {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Accept": "application/json",
-        },
-      })
-      .then((response) => {
-        // console.log(response);
-        return response.json();
-      },
-      )
-      .then((data) => {
-        // console.log(data);
-        
-        setTemperatureSet(data.temperature);
-      })
-      .catch((error) => {
-        console.error("Could not get data from server");
-        console.error(error);
-      });
-
-  },[]);
-
-  useEffect(() => {
-
-    fetch(baseUrl + "/data",
-      {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Accept": "application/json",
-        },
-      })
-      .then((response) => {
-        // console.log(response);
-        return response.json();
-      },
-      )
-      .then((data) => {
-        // console.log(data);
-        
-        setHouseTemp(data.temperature);
-        setPressure(data.pressure);
-        setHumidity(data.humidity);
-      })
-      .catch((error) => {
-        console.error("Could not get data from server");
-        console.error(error);
-      });
-
-  },[]);
 
   return (
     <div className="App">
+      <Router>
 
-      <div id="houseInfoContainer">
+        <Routes>
+          <Route exact path="/" element={<HomePage />}/>
 
-        <p>
-          {
-            "Humidity: " + humidity
-          }
-        </p>
+          <Route path="*" element={<h4>Page not found</h4>} />
+        </Routes>
 
-        <p>
-          {
-            "Pressure: " + pressure
-          }
-        </p>
-
-      </div>
-
-      <div id="houseThemperatureContainer">
-
-        <h4>
-          {
-            "House themperature: " + houseTemp
-          }
-        </h4>
-
-      </div>
-
-      <div id="themperatureSetContainer">
-        <h4>
-          {
-            "Wanted themperature: " + temperatureSet
-          }
-        </h4>
-      </div>
-
+      </Router>
     </div>
   );
 }
