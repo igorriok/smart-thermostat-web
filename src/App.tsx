807a58bb06827b5fc9994ac3984c7fb4cd53d231
Igor <i.solonari@gmail.com>
2021-12-10 10:36:03 +0000

    added typescript and eslint

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import HomePage from './pages/Home';
+
+
+function App() {
+	return (
+		<div className="App">
+			<Router>
+
+				<Routes>
+					<Route path="/" element={<HomePage />}/>
+
+					<Route path="*" element={<h4>Page not found</h4>} />
+				</Routes>
+
+			</Router>
+		</div>
+	);
+}
+
+export default App;
